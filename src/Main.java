11f75e4ee18b109a03d1a22a70e56ed40d1be74d
shashank.agrawal <shashank.agrawal@metricstream.com>
2022-09-19 16:53:22 +0530

    adding demo file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello there");
+        demo d = new demo();
+        d.printHello();
     }
 }
---
+++ src/demo.java
@@ -0,0 +1,5 @@
+public class demo {
+    public void printHello(){
+        System.out.println("Hello world!");
+    }
+}
